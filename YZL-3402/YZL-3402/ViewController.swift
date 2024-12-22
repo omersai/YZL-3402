1704258bfe6254efbb4df4ad9d3e045c99c3e32d
Ömer Saitoğlu <omer.saitoglu>
2024-12-22 13:58:53 +0300

    Created Project

---
+++ YZL-3402/YZL-3402/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YZL-3402
+//
+//  Created by Ömer Saitoğlu on 22.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
