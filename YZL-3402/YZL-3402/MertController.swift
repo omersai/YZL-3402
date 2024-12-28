8334206e7efd148e89a2f45b0ebeff5533e6e952
mertziya <mert7ziya@gmail.com>
2024-12-28 11:17:09 +0300

    Added Login Method

---
+++ YZL-3402/YZL-3402/MertController.swift
@@ -0,0 +1,20 @@
+//
+//  MertController.swift
+//  YZL-3402
+//
+//  Created by Mert Ziya on 28.12.2024.
+//
+
+import Foundation
+
+import UIKit
+
+class MertController : UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    private func login(){
+        print("Login Print")
+    }
+}
