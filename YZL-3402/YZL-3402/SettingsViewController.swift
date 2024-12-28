dff747ecf8ab2a8deb4af5865322a153c47a27d3
Ömer Saitoğlu <omer.saitoglu>
2024-12-28 10:30:20 +0300

    Added Settings Controller, Updated ViewController

---
+++ YZL-3402/YZL-3402/SettingsViewController.swift
@@ -0,0 +1,27 @@
+//
+//  SettingsViewController.swift
+//  YZL-3402
+//
+//  Created by Ömer Saitoğlu on 28.12.2024.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
